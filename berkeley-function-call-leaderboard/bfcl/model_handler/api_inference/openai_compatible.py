511e5ecb50e4ec352af366b333d32a36c1e0deba
jhpiedrahitao <jhpiedrahitao97@gmail.com>
2025-05-30 12:33:52 -0500

    update to openai compatible api inference module

--- berkeley-function-call-leaderboard/bfcl/model_handler/api_inference/openai_compatible.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/api_inference/openai_compatible.py
@@ -7,13 +7,30 @@ from bfcl.model_handler.utils import (
     retry_with_backoff,
 )
 
-class SambaCloudHandler(OpenAIHandler):
+class OpenaiCompatibleHandler(OpenAIHandler):
     def __init__(self, model_name, temperature) -> None:
+        self.compatible_provider=model_name.split('/')[0]
+        model_name = '/'.join([sec for sec in model_name.split('/')[1:]])
         super().__init__(model_name, temperature)
     
     def _init_client(self):
-        base_url = "https://api.sambanova.ai/v1"
-        self.client = OpenAI(base_url=base_url, api_key=os.getenv("SAMBACLOUD_API_KEY"))
+        if self.compatible_provider == "sambanova":
+            base_url = "https://api.sambanova.ai/v1"
+            self.client = OpenAI(base_url=base_url, api_key=os.getenv("SAMBACLOUD_API_KEY"))
+        elif self.compatible_provider == "groq":
+            base_url = "https://api.groq.com/openai/v1"
+            self.client = OpenAI(base_url=base_url, api_key=os.getenv("GROQ_API_KEY"))
+        elif self.compatible_provider == "cerebras":
+            base_url = "https://api.cerebras.ai/v1"
+            self.client = OpenAI(base_url=base_url, api_key=os.getenv("CEREBRAS_API_KEY"))
+        elif self.compatible_provider == "fireworks":
+            base_url = "https://api.fireworks.ai/inference/v1"
+            self.client = OpenAI(base_url=base_url, api_key=os.getenv("FIREWORKS_API_KEY"))
+        elif self.compatible_provider == "together":
+            base_url = "https://api.together.xyz/v1"
+            self.client = OpenAI(base_url=base_url, api_key=os.getenv("TOGETHER_API_KEY"))
+        else:
+            raise(Exception(f"{self.compatible_provider} not implemented"))
 
     @retry_with_backoff(error_type=RateLimitError)
     def generate_with_backoff(self, **kwargs):
